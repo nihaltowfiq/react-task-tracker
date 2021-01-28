a0ccec0d63afa0c36e1fdd58321db8659ed2fb72
nihaltowfiq <nihaltowfiq@gmail.com>
2021-01-28 23:55:46 +0600

    static tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor's Appointment",
+      day: "Feb 5th at 2:30PM",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Engineer's Appointment",
+      day: "Feb 5th at 2:30PM",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shoppong",
+      day: "Feb 5th at 2:30PM",
+      reminder: false,
+    },
+  ]);
   return (
     <div className="container">
       <Header title="Tasks Tracker" />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// const tasks = ;
+
+const Tasks = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
