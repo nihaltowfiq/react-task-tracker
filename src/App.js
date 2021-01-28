533b4b4482174c3771ad593abd4eb2223f59a205
nihaltowfiq <nihaltowfiq@gmail.com>
2021-01-29 00:32:28 +0600

    toogleReminder added

--- src/App.js
+++ src/App.js
@@ -24,8 +24,18 @@ function App() {
     },
   ]);
 
+  // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
   return (
     <div className="container">
@@ -33,7 +43,7 @@ function App() {
       {tasks.length === 0 ? (
         "No Task to Show"
       ) : (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       )}
     </div>
   );
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,11 +3,16 @@ import Task from "./Task";
 
 // const tasks = ;
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={onDelete} />
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
